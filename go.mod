8b0b7c0bcb08567a3808704bb51eff8a24969f16
Paul Easter <paul.john.easter@gmail.com>
2021-12-21 15:11:20 +1100

    Working KNN

--- go.mod
+++ go.mod
@@ -2,22 +2,20 @@ module goml
 
 go 1.17
 
-require (
-	github.com/go-gota/gota v0.11.0
-	github.com/sjwhitworth/golearn v0.0.0-20210906182158-947ee7214ee5
-)
+require github.com/sjwhitworth/golearn v0.0.0-20211014193759-a8b69c276cd8
 
 require (
 	github.com/gonum/blas v0.0.0-20181208220705-f22b278b28ac // indirect
 	github.com/gonum/lapack v0.0.0-20181123203213-e4cdc5a0bff9 // indirect
 	github.com/gonum/matrix v0.0.0-20181209220409-c518dec07be9 // indirect
-	github.com/google/go-cmp v0.4.0 // indirect
-	github.com/guptarohit/asciigraph v0.5.1 // indirect
-	github.com/mattn/go-runewidth v0.0.7 // indirect
-	github.com/olekukonko/tablewriter v0.0.4 // indirect
-	github.com/rocketlaunchr/dataframe-go v0.0.0-20201007021539-67b046771f0b // indirect
-	golang.org/x/exp v0.0.0-20200331195152-e8c3332aa8e5 // indirect
-	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/guptarohit/asciigraph v0.5.2 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rocketlaunchr/dataframe-go v0.0.0-20211025052708-a1030444159b // indirect
+	golang.org/x/exp v0.0.0-20211216164055-b2b84827b756 // indirect
 	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
-	gonum.org/v1/gonum v0.9.1 // indirect
+	gonum.org/v1/gonum v0.9.3 // indirect
+	gonum.org/v1/netlib v0.0.0-20210927171344-7274ea1d1842 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,4 +1,5 @@
 // Code snippets take from this tutorial: https://towardsdatascience.com/golang-for-machine-learning-bd4bb84594ee
+// Same as here with minor changes: https://dev.to/salah856/build-a-simple-knn-model-using-golang-34db
 
 package main
 
@@ -13,31 +14,45 @@ import (
 	"github.com/sjwhitworth/golearn/knn"
 )
 
-func main() {
-	fmt.Println("Load our csv data")
-	rawData, err := base.ParseCSVToInstances("../data/iris_headers.csv", true)
+func check(err error, s string) {
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("%s: %s", s, err.Error()))
 	}
+}
+
+func main() {
+	// irisCsv, err := os.Open("./data/iris_headers.csv")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// df := dataframe.ReadCSV(irisCsv)
+	// fmt.Println(df)
+
+	fmt.Println("Load our csv data")
+	rawData, err := base.ParseCSVToInstances("./data/iris_headers.csv", true)
+	check(err, "Problem loading csv file")
 
 	fmt.Println("Initialize our KNN classifier")
 	cls := knn.NewKnnClassifier("euclidean", "linear", 2)
 
 	fmt.Println("Perform a training-test split")
+
 	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.50)
+
 	cls.Fit(trainData)
 
 	fmt.Println("Calculate the euclidian distance and return the most popular label")
+
 	predictions, err := cls.Predict(testData)
-	if err != nil {
-		panic(err)
-	}
+	check(err, "Problem calculating predictions")
+
 	fmt.Println(predictions)
 
 	fmt.Println("Print our summary metrics")
 	confusionMat, err := evaluation.GetConfusionMatrix(testData, predictions)
-	if err != nil {
-		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
-	}
+
+	check(err, "Problem calculating confusion matrix")
+
 	fmt.Println(evaluation.GetSummary(confusionMat))
 }
